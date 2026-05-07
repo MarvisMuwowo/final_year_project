376ba77a634acc36a1a0ddded3e832fb86ef0ed4
marvis muwowo <muwowomarvis@gmail.com>
2026-05-08 01:56:12 +0200

    clean structure: add ignore rules and project files

---
+++ frontend/componets/learning.py
@@ -0,0 +1,8 @@
+import dash
+from dash import dcc, html, dash_table, Input, Output
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+import requests
+import pandas as pd
+
